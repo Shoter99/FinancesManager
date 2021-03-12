934fbe83c344ad10b076e4340e0d43237ef651b3
Shoter99 <shoter998@gmail.com>
2021-03-12 21:52:08 +0100

    Need to add many to one relation database

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, redirect, url_for, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 import os, datetime
-
+from statistic import plot
 UPLOAD_FOLDER = os.path.abspath('static')
 
 app = Flask(__name__)
+app.register_blueprint(plot)
 app.secret_key = "gaighaoehghwah"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///finances.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -20,27 +21,33 @@ def saveDate():
         quit()
     dateNow = datetime.datetime.now()
     f.write(dateNow.strftime("%m.%d,"))
-
+def calculateSum(exp):
+    sum = 0
+    for i in exp:
+        if i.char == "+":
+            sum+=i.price
+        else:
+            sum-=i.price
+    return sum
 def saveToFile(exp):
     try:
         f= open('data.txt', 'a')
     except:
         print('nie udało się otworzyć pliku')
         quit()
-    sum = 0
-    for i in exp:
-        sum+=i.price
-    f.write(str(sum)+',')
+    f.write(str(calculateSum(exp))+',')
     f.close()
 
 class Expenses(db.Model):
     _id = db.Column('id', db.Integer, primary_key=True)
     name = db.Column(db.String(120))
+    char = db.Column(db.String(10))
     price = db.Column(db.Float)
     path_to_img = db.Column(db.String(300))
 
-    def __init__(self,name,price,path_to_img):
+    def __init__(self,name,char,price,path_to_img):
         self.name = name
+        self.char = char
         self.price = price
         self.path_to_img = path_to_img
 
@@ -49,6 +56,7 @@ class Expenses(db.Model):
 def home():
     if request.method == 'POST':
         name = request.form['name']
+        char = request.form['value']
         price = request.form['price']
         img = request.files['img']
         if img.filename == "":
@@ -56,7 +64,7 @@ def home():
         if img:
             filename = secure_filename(img.filename)
             img.save(os.path.join(app.config['UPLOAD_FOLDER'],'images', filename))
-        expenses = Expenses(name,price, img.filename)
+        expenses = Expenses(name, char, price, img.filename)
         db.session.add(expenses)
         db.session.commit()
         exp = Expenses.query.all()
@@ -66,9 +74,8 @@ def home():
     else:
         all_spendings = 0
         exp = Expenses.query.all()
-        for p in exp:
-            all_spendings+=p.price
-        return render_template('wydatki.html',all_spendings=round(all_spendings,2),expen = Expenses, expenses=exp, path=os.path.join('images',""))
+        sum = calculateSum(exp)
+        return render_template('wydatki.html',all_spendings=round(sum,2),expen = Expenses, expenses=exp, path=os.path.join('images',""))
 
 @app.route('/deleteRecord', methods=["POST", "GET"])
 def deleteRecord():
@@ -81,7 +88,7 @@ def deleteRecord():
     return redirect(url_for("home"))
 @app.route('/<page>')
 def dif(page):
-    return '<center><h1>'+page+' is not working!</h1></center>'
+    return '<center><h1>strona '+page+' nie jest jeszcze skończona</h1></center>'
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True)
--- statistic.py
+++ statistic.py
@@ -1,10 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy
+from flask import Blueprint, Flask, url_for, redirect
 
-data = numpy.genfromtxt("data.txt", delimiter=',')
-date = numpy.genfromtxt("date.txt", delimiter=',', dtype="|S5")
-print(data)
-print(date)
-plt.plot(date,data)
-plt.show()
+plot = Blueprint('plot', __name__, static_folder="static", template_folder="templates")
+
+@plot.route("/podsumowanie")
+def podsumowanie():
+    data = numpy.genfromtxt("data.txt", delimiter=',')
+    date = numpy.genfromtxt("date.txt", delimiter=',', dtype="|S5")
+    print(data)
+    print(date)
+    plt.plot(date,data)
+    plt.show()
+    return redirect(url_for("home"))
 
